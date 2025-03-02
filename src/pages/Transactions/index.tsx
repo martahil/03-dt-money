fba29fdcb3c53a8cf7fdd0b10d7bddb2d0c49f8b
Marta Hilgert <marta.hil.1988@gmail.com>
2025-03-02 18:00:06 -0300

    Feat: implement TransactionsContext, update Transactions component with the context and show income, outcome, and total (Summary)

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,32 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import {  PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
-
-interface Transactions {
-  id: number;
-  description: string;
-  type: 'income' | 'outcome';
-  price: number;
-  category: string;
-  createdAt: string;
-}
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transactions[]>([])
+  const { transactions } = useContext(TransactionsContext)
 
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3000/transactions')
-    const data = await response.json()
-
-    setTransactions(data)
-  }
-
-  useEffect(() => {
-    loadTransactions()
-  }, [])
- 
   return(
     <div>
       <Header />
